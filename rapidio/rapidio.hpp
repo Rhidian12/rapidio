6f95bfe04275e5d5df52471b5d770be930b1ed94
Rhidian De Wit <rhidiandewit@gmail.com>
2024-06-05 12:49:02 +0200

    continue work on Win32 implementation

--- rapidio/rapidio.hpp
+++ rapidio/rapidio.hpp
@@ -16,20 +16,21 @@
 #endif // RAPIDIO_USE_EXCEPTIONS
 
 #include <filesystem>
+#include <optional>
 #include <string>
+#include <string_view>
 
 namespace rapidio
 {
-	enum class FileAccessMode
+	enum class FileAccessMode : uint32_t
 	{
 		#ifdef _WIN32
-		Read = GENERIC_READ,
-		Write = GENERIC_WRITE,
-		ReadWrite = Read | Write
+		ReadOnly = GENERIC_READ,
+		ReadWrite = ReadOnly | GENERIC_WRITE
 		#endif // _WIN32
 	};
 
-	enum class FileOpenMode
+	enum class FileOpenMode : uint32_t
 	{
 		#ifdef _WIN32
 		CreateNew = CREATE_NEW,
@@ -51,23 +52,66 @@ namespace rapidio
 	class FileView final
 	{
 	public:
-		FileView(const std::string& InFilepath, const FileAccessMode AccessMode, const FileOpenMode OpenMode);
+		static std::optional<FileView> CreateFileView(const std::string& InFilepath, const FileAccessMode AccessMode, const FileOpenMode OpenMode,
+			const std::optional<size_t>& BytesToRead);
+
+		std::string_view GetFileContents();
 
 	private:
+		FileView(const std::string& InFilepath);
 		bool OpenFile(const FileAccessMode AccessMode, const FileOpenMode OpenMode);
+		bool GetFilesize();
+		bool CreateFileMappingHandle(const FileAccessMode AccessMode, const std::optional<size_t>& BytesToRead);
+		bool CreateMapViewOfFile(const FileAccessMode AccessMode, const std::optional<size_t>& BytesToRead);
+		bool GetSystemAllocationGranularity();
 
 		std::string Filepath;
+		int64_t Filesize;
 		
 		#ifdef _WIN32
-		Win32Handle Handle;
+		Win32Handle FileHandle;
+		Win32Handle FileMappingHandle;
+		Win32Handle MappedViewHandle;
+		DWORD AllocationGranularity;
 		#endif // _WIN32
 	};
 
-	FileView::FileView(const std::string& InFilepath, const FileAccessMode AccessMode, const FileOpenMode OpenMode)
-		: Filepath(InFilepath)
+	std::optional<FileView> FileView::CreateFileView(const std::string& InFilepath, const FileAccessMode AccessMode, const FileOpenMode OpenMode,
+		const std::optional<size_t>& BytesToRead)
 	{
-		OpenFile(AccessMode, OpenMode);
+		FileView View(InFilepath);
+
+		if (!View.OpenFile(AccessMode, OpenMode))
+		{
+			return std::nullopt;
+		}
+
+		if (!View.GetFilesize())
+		{
+			return std::nullopt;
+		}
+
+		if (!View.CreateFileMappingHandle(AccessMode, BytesToRead))
+		{
+			return std::nullopt;
+		}
+
+		if (!View.GetSystemAllocationGranularity())
+		{
+			return std::nullopt;
+		}
+
+		return View;
 	}
+
+	std::string_view FileView::GetFileContents()
+	{
+		return std::string_view();
+	}
+
+	FileView::FileView(const std::string& InFilepath)
+		: Filepath(InFilepath)
+	{}
 
 	bool FileView::OpenFile(const FileAccessMode AccessMode, const FileOpenMode OpenMode)
 	{
@@ -124,7 +168,7 @@ namespace rapidio
 				break;
 		}
 
-		Handle = CALL_WIN32_RV_IGNORE_ERROR
+		FileHandle = CALL_WIN32_RV_IGNORE_ERROR
 		(
 			CreateFileA
 			(
@@ -139,6 +183,71 @@ namespace rapidio
 			ErrorToIgnore
 		);
 
+		return FileHandle != nullptr;
+		#endif // _WIN32
+	}
+
+	bool FileView::GetFilesize()
+	{
+		#ifdef _WIN32
+		LARGE_INTEGER InFilesize;
+		const BOOL Ret{ CALL_WIN32_RV(GetFileSizeEx(FileHandle, &InFilesize)) };
+		Filesize = InFilesize.QuadPart;
+		return Ret != 0;
+		#endif // _WIN32
+	}
+
+	bool FileView::CreateFileMappingHandle(const FileAccessMode AccessMode, const std::optional<size_t>& BytesToRead)
+	{
+		#ifdef _WIN32
+		constexpr DWORD ReadOnlyAccess = PAGE_READONLY;
+		constexpr DWORD ReadWriteAccess = PAGE_READWRITE;
+		const DWORD Size{ BytesToRead.value_or(0) };
+
+		FileMappingHandle = CALL_WIN32_RV
+		(
+			CreateFileMappingA
+			(
+				FileHandle,
+				nullptr,
+				AccessMode == FileAccessMode::ReadOnly ? ReadOnlyAccess : ReadWriteAccess,
+				HIWORD(Size), // If 'Size' is 0, we read the entire file
+				LOWORD(Size),
+				"") // [TODO]: assign a name here to ensure we don't re-create file mappings
+		);
+
+		return FileMappingHandle != nullptr;
+		#endif // _WIN32
+	}
+
+	bool FileView::CreateMapViewOfFile(const FileAccessMode AccessMode, const std::optional<size_t>& BytesToRead)
+	{
+		#ifdef _WIN32
+		constexpr DWORD ReadOnlyAccess = FILE_MAP_READ;
+		constexpr DWORD ReadWriteAccess = FILE_MAP_ALL_ACCESS;
+		constexpr DWORD Size{};
+		MappedViewHandle = CALL_WIN32_RV
+		(
+			MapViewOfFile
+			(
+				FileMappingHandle,
+				AccessMode == FileAccessMode::ReadOnly ? ReadOnlyAccess : ReadWriteAccess,
+				484341,
+				484341,
+				Size // 0 means it will create a view of the entire mapped file
+			)
+		);
+
+		return MappedViewHandle != nullptr;
+		#endif // _WIN32
+	}
+
+	bool FileView::GetSystemAllocationGranularity()
+	{
+		#ifdef _WIN32
+		SYSTEM_INFO SystemInfo;
+		CALL_WIN32(GetNativeSystemInfo(&SystemInfo));
+		AllocationGranularity = SystemInfo.dwAllocationGranularity;
 		return true;
 		#endif // _WIN32
 	}
